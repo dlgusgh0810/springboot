7add7f2356121f165e422dd3ccbcf74582812a74
COM4-main\User <bcucs20@gmail.com>
2025-06-05 09:53:01 +0900

    일단 커밋 스텝2번

--- SpringLect1/src/main/java/com/Lect/week09/BasicProcessController1.java
+++ SpringLect1/src/main/java/com/Lect/week09/BasicProcessController1.java
@@ -1,9 +1,8 @@
 package com.Lect.week09;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("regist")
@@ -12,5 +11,16 @@ public class BasicProcessController1 {
     public String detail(@PathVariable("id") Long stepId) {
         String viewName = "registerStep" + stepId;
         return "week09/" + viewName;
+    }
+    @PostMapping("/step2")
+    public String handleStep2(@RequestParam("view") String view,
+                              @RequestParam(value="agree", defaultValue = "false")
+                                Boolean agree,
+                              RedirectAttributes redirectAttributes) {
+        if(!agree) {
+            redirectAttributes.addFlashAttribute("message", "약관 동의를 해주세요.");
+            return "redirect:http://localhost:8080/regist/step/1";
+        }
+        return "week09/" + view;
     }
 }
